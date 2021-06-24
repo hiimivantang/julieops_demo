49c33aca3b9d5569c745039e18262680ab9e501f
Ivan Tang <hiimivantang@gmail.com>
2021-06-24 14:05:22 +0800

    wip

---
+++ post-success-failure.sh
@@ -0,0 +1,6 @@
+curl "https://api.github.com/repos/hiimivantang/kdb-demo/statuses/$GITHUB_PR_HEAD_SHA?access_token=$GITHUB_TOKEN" \
+      -H "Content-Type: application/json" \
+      -X POST \
+      -d "{\"state\": \"success\",\"context\": \"topology-builder-pipeline-verify\", \"description\": \"Jenkins\", \"target_url\": \"http://ec2-54-179-227-183.ap-southeast-1.compute.amazonaws.com:8081/job/topology-builder-pipeline-verify/$BUILD_NUMBER/console\"}"
+
+exit 0
