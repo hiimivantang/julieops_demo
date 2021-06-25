60f35c06ccd383f45bc7342d5ff330b047b25552
Ivan Tang <hiimivantang@gmail.com>
2021-06-25 23:44:58 +0800

    removed unnecessary scripts

--- post-hook-failure.sh
+++
@@ -1,6 +0,0 @@
-curl "https://api.github.com/repos/hiimivantang/kdb-demo/statuses/$GITHUB_PR_HEAD_SHA?access_token=$GITHUB_TOKEN" \
-      -H "Content-Type: application/json" \
-        -X POST \
-          -d "{\"state\": \"failure\",\"context\": \"topology-builder-pipeline-verify\", \"description\": \"Jenkins\", \"target_url\": \"http://ec2-54-179-227-183.ap-southeast-1.compute.amazonaws.com:8081/job/topology-builder-pipeline-verify/$BUILD_NUMBER/console\"}"
-
-exit 1
