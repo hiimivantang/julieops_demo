5949822b65ca2b89d8cdd161de53f881f60755fd
Ivan Tang <hiimivantang@gmail.com>
2021-06-25 13:57:44 +0800

    wip

--- checks/verify-num-of-partitions.sh
+++ checks/verify-num-of-partitions.sh
@@ -7,21 +7,12 @@ required=`cat $FILE | grep num.partitions | wc -l`
 matches=`cat $FILE | grep num.partitions | grep $REQUIRED_FACTOR | wc -l`
 
 
-#TODO: SCAN AND CHECK EXISTING TOPICS
-
-if [ "$matches" -ge "$required" ]  ;
-  then
-         echo -e "all topics have the required minimum num of partitions"
-            exit 0
+if [ "$matches" -ne "$required" ] ;
+then
+       echo -e "Some topics have not the minimum num of partitions, please review"
+          exit 1
+      elif [ "$matches" -ge "$required" ]  ;
+      then
+             echo -e "all topics have the required minimum num of partitions"
+                exit 0
 fi
-
-
-#if [ "$matches" -lt "$required" ] ;
-#then
-#       echo -e "Some topics have not the minimum num of partitions, please review"
-#          exit 1
-#      elif [ "$matches" -ge "$required" ]  ;
-#      then
-#             echo -e "all topics have the required minimum num of partitions"
-#                exit 0
-#fi
